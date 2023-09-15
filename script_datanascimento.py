3169921ff6390da838d1312829c22ab934b8e1b2
awmarques <139471143+awmarques@users.noreply.github.com>
2023-09-15 11:34:07 -0300

    Create script_datanascimento.py

---
+++ script_datanascimento.py
@@ -0,0 +1,4 @@
+dia=input('Digite o dia que você nasceu')
+mes=input('Digite o mês que voce nasceu')
+ano=input('Digite o ano que voce nasceu')
+print ('Você nasceu no dia '+ dia + ' do mês de '+ mes +  ' do ano de ' + ano)
