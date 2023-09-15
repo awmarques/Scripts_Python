4748cb2c87a0702307786ad0d302f9d838c6a604
awmarques <139471143+awmarques@users.noreply.github.com>
2023-09-15 11:48:53 -0300

    Create soma_doisnumeros.py

---
+++ soma_doisnumeros.py
@@ -0,0 +1,7 @@
+print('Função para somar dois números!!!')
+numero1 = input  ('Digite o primeiro numero:')
+numero2 = input  ('Digite o seguindo numero:')
+numero1 = int(numero1)
+numero2 = int(numero2)
+soma = numero1+numero2
+print ('A soma do número' ,numero1, ' com o número ' ,numero2, ' é: ', soma)
